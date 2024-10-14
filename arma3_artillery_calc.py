c6d7b304ce829161943cc9ef80da3af05373f320
name2965 <66663783+name2965@users.noreply.github.com>
2024-10-15 03:23:07 +0900

    Update arma3_artillery_calc.py
    Replaced the calculation equation and made general improvements.

--- arma3_artillery_calc.py
+++ arma3_artillery_calc.py
@@ -1,12 +1,9 @@
 from fractions import Fraction
 import numpy as np
 import sympy as s
-import math as m
 
-velocity_table = [242.9,388.8,649]
+velocity_table = [153.9,243,388.8,648,810]
 charge_level = -1
-t = s.Symbol('t', real=True)
-theta = s.Symbol('theta', real=True)
 
 def artillery_calc_mod1():
 
@@ -18,24 +15,39 @@ def artillery_calc_mod1():
     y2 = int(input('target y-axis coordinate > '))
     z2 = int(input('target altitude > '))
 
-    angle = m.atan2((x1 - x2), (y1 - y2)) * float(Fraction(180, Fraction(np.pi)) * Fraction(6400, 360))
+    angle = s.atan2((x1 - x2), (y1 - y2)) * float(Fraction(180, Fraction(np.pi)) * Fraction(6400, 360))
     rx = round(np.sqrt(((x1 - x2)**2) + ((y1 - y2)**2)))
 
-    if rx % 10 != 0:
-        rx += 10 - (rx%10)
-
-    if rx < 1400 or rx > 29900:
+    if rx < 300 or rx > 29900:
         print('invalid distance')
+        print('distance : ' + str(rx))
         return
-    elif rx >= 1400 and rx < 6000:
+    elif rx >= 300 and rx < 2400:
+        charge_level = 0
+        v = velocity_table[charge_level]
+    elif rx >= 2400 and rx < 6000:
         charge_level = 1
-        v = velocity_table[charge_level-1]
+        v = velocity_table[charge_level]
     elif rx >= 6000 and rx < 15400:
         charge_level = 2
-        v = velocity_table[charge_level-1]   
+        v = velocity_table[charge_level]
     elif rx >= 15400 and rx <= 29900:
         charge_level = 3
-        v = velocity_table[charge_level-1]
+        v = velocity_table[charge_level]
+
+    v2 = v*v
+    v4 = v2*v2
+    rx2 = rx*rx
+    g = 9.80665
+    ry = z1 - z2
+
+    res_h = s.atan((v2 + s.sqrt(v4 - g * ((g * rx2) + (2 * ry * v2)))) / (g * rx))
+    res_l = s.atan((v2 - s.sqrt(v4 - g * ((g * rx2) + (2 * ry * v2)))) / (g * rx))
+
+    sa = res_h * 180 / np.pi
+    sk = res_l * 180 / np.pi
+    eta_l = round((rx / (v * s.cos(sk*np.pi/180))), 1)
+    eta_h = round((rx / (v * s.cos(sa*np.pi/180))), 1)
 
     if angle > 0:
         an = 3200 - angle
@@ -43,75 +55,65 @@ def artillery_calc_mod1():
     elif angle < 0:
         angle = 3200 + angle
 
-    ax = np.pi * 0.00085 * 1.225 * 0.155**2 * float(Fraction(Fraction(v**2), (8 * 45)))
-    ay = np.pi * 0.00085 * 1.225 * 0.155**2 * float(Fraction(Fraction(v**2), (8 * 45)))
+    sk *= (6400/360)
+    sa *= (6400/360)
 
-    eq1 = rx - (5 * s.cos(theta) + v * s.cos(theta) * t - (1/2) * ax * s.cos(theta) * t**2)
-    eq2 = z2 - ((z1 + 5*s.sin(theta)) + v * s.sin(theta) * t - (1/2) * (ay * s.sin(theta) + (980665/100000)) * t**2)
-
-    res = s.solve((eq1, eq2), (theta, t), dict=True)
-    
-    for i in range(len(res)):
-        if res[i][t] >= 50.0 or res[i][t] < 1:
-            continue
-        sk = res[i][theta] * float(Fraction(180, Fraction(np.pi)) * Fraction(6400, 360))
-        sa = 1600 - sk
-
-        if sk >= sa:
-            continue
-
-        print()
-        print('[+] Shooting Angle : ' + str(round(angle)))
-        print('[+] Low angle elevation : ' + str(round(sk, 1)))
-        print('[+] ETA : ' + str(round(res[i][t], 1)))
-        print('[+] Charge level : ' + str(charge_level))
-        print()
-
+    print()
+    print('[+] Shooting Angle : ' + str(round(angle)))
+    print('[+] Low angle elevation : ' + str(round(sk)))
+    print('[+] Low ELEV ETA : ' + str(eta_l))
+    print('[+] High angle elevation : ' + str(round(sa)))
+    print('[+] High ELEV ETA : ' + str(eta_h))
+    print('[+] Charge level : ' + str(charge_level))
+    print()
 
 def artillery_calc_mod2():
 
-    rx = int(input('distance between you and the target (minimum = 1400) > '))
+    rx = int(input('distance between you and the target (minimum = 1800) > '))
     z1 = int(input('your altitude > '))
     z2 = int(input('target altitude > '))
 
-    if rx % 10 != 0:
-        rx += 10 - (rx%10)
-
-    if rx < 1400 or rx > 29900:
+    if rx < 300 or rx > 29900:
         print('invalid distance')
+        print('distance : ' + str(rx))
         return
-    elif rx >= 1400 and rx < 6000:
+    elif rx >= 300 and rx < 2400:
+        charge_level = 0
+        v = velocity_table[charge_level]
+    elif rx >= 2400 and rx < 6000:
         charge_level = 1
-        v = velocity_table[charge_level-1]
+        v = velocity_table[charge_level]
     elif rx >= 6000 and rx < 15400:
         charge_level = 2
-        v = velocity_table[charge_level-1]   
+        v = velocity_table[charge_level]
     elif rx >= 15400 and rx <= 29900:
         charge_level = 3
-        v = velocity_table[charge_level-1]
+        v = velocity_table[charge_level]
 
-    ax = np.pi * 0.00085 * 1.225 * 0.155**2 * float(Fraction(Fraction(v**2), (8 * 45)))
-    ay = np.pi * 0.00085 * 1.225 * 0.155**2 * float(Fraction(Fraction(v**2), (8 * 45)))
+    v2 = v*v
+    v4 = v2*v2
+    rx2 = rx*rx
+    g = 9.80665
+    ry = z1 - z2
 
-    eq1 = rx - (5 * s.cos(theta) + v * s.cos(theta) * t - (1/2) * ax * s.cos(theta) * t**2)
-    eq2 = z2 - ((z1 + 5*s.sin(theta)) + v * s.sin(theta) * t - (1/2) * (ay * s.sin(theta) + (980665/100000)) * t**2)
+    res_h = s.atan((v2 + s.sqrt(v4 - g * ((g * rx2) + (2 * ry * v2)))) / (g * rx))
+    res_l = s.atan((v2 - s.sqrt(v4 - g * ((g * rx2) + (2 * ry * v2)))) / (g * rx))
 
-    res = s.solve((eq1, eq2), (theta, t), dict=True)
+    sa = res_h * 180 / np.pi
+    sk = res_l * 180 / np.pi
+    eta_l = round((rx / (v * s.cos(sk*np.pi/180))), 1)
+    eta_h = round((rx / (v * s.cos(sa*np.pi/180))), 1)
 
-    for i in range(len(res)):
-        if res[i][t] >= 50.0 or res[i][t] < 1:
-            continue
-        sk = res[i][theta] * float(Fraction(180, Fraction(np.pi)) * Fraction(6400, 360))
-        sa = 1600 - sk
+    sk *= (6400/360)
+    sa *= (6400/360)
 
-        if sk >= sa:
-            continue
-
-        print()
-        print('[+] Low angle elevation : ' + str(round(sk,8)))
-        print('[+] ETA : ' + str(round(res[i][t], 1)))
-        print('[+] Charge level : ' + str(charge_level))
-        print()
+    print()
+    print('[+] Low angle elevation : ' + str(round(sk)))
+    print('[+] Low ELEV ETA : ' + str(eta_l))
+    print('[+] High angle elevation : ' + str(round(sa)))
+    print('[+] High ELEV ETA : ' + str(eta_h))
+    print('[+] Charge level : ' + str(charge_level))
+    print()
 
 if __name__ == '__main__':
 
@@ -126,6 +128,9 @@ if __name__ == '__main__':
         artillery_calc_mod1()
     elif mode == 2:
         artillery_calc_mod2()
+    else:
+        print('[+] invalid input')
+        print('[+] please input 1 or 2')
     else:
         print('[+] invalid input')
         print('[+] please input 1 or 2')
